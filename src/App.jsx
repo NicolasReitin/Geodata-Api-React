576bd107b6d409969af6ba50f8324f3b146e8ca3
Nicolas REITIN <102169931+NicolasReitin@users.noreply.github.com>
2024-02-02 16:52:27 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -28,7 +28,7 @@ function App() {
   return (
     <div className="min-h-screen bg-slate-800">
       <div className="max-w-7xl mx-auto py-20 px-4">
-        <h1 className="text-gray-50 text-4xl">Europte Countries Data.</h1>
+        <h1 className="text-gray-50 text-4xl">Europe Countries Data</h1>
         <p className="text-gray-100 text-xl mb-8">Click on a card to reveal a country's information.</p>
         {countries && (
           <ul className="grid min-[450px]:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-10 auto-rows-[200px]">
